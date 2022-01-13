d4e639d600709f9f1478c8a8acf6507b2c38da08
Eastern Lai <easternlai@Easterns-MacBook-Pro.local>
2022-01-13 01:00:24 -0800

    added content for projects page

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React from "react";
+import cultree from "../images/cultree.png";
 
 const ProjectPage = () => {
+  return (
+    <div className="projects-page">
+      <div className="text-large mb-10">Cultree Project</div>
+      <div className="text-med">about </div>
+      <div className="projects-page__wrapper">
+      <div className="ml-15">
+        Cultree is a multi-tenant, mern application to helps companies improve
+        their employee engagement with event signups, swag store, and company
+        photo galleries with many more features to come!
+      </div>
+      <img src={cultree} className="projects-page__wrapper--image"/>
+      </div>
 
-    return (
-        <div>
-        <div className="text-large mb-10">Cultree Project</div>
-        <div className="text-med">about </div>
-        <div className='ml-15'>Cultree is a multi-tenant, mern application that provides a central web location for office managers and company cultree committee liasons to create event signups, offer swag and gifts via a store, and post company photo galleries with many more features to come! </div>
 
-        <div className="text-med  mt-20">Technologies </div>
-        <div className='ml-15 mb-10'><span className="bold">frontend - </span> </div>
-        <div className='ml-15 mb-10'><span classNams="bold">backend - </span>  </div>
-        <div className='ml-15 mb-10'><span className="bold">devops - </span>Cultree's infrastructure resides in an AWS  Linux EC2 instance that maintains the frontend, backend, mongoDB, and apache containers. Terraform is used to build and destroy the AWS environment and docker compose is use to automate the build and run of the containers.</div>
+      <div className="text-med  mt-20">Technologies </div>
+      <div className="ml-15 mb-10">
+        <span className="bold">frontend - </span>The frontend was built with
+        ReactJs and uses SCSS for styling. Redux is used to manage the global
+        state and the useReducer and useState hooks are used for component state
+        management as needed.
+      </div>
+      <div className="ml-15 mb-10">
+        <span className="bold">backend - </span>
 
-        </div>
-    )
+      </div>
+      <div className="ml-15 mb-10">
+        <span className="bold">devops - </span>
+        Cultree's infrastructure resides in an AWS Linux EC2 instance that
+        maintains  four docker containers: Node:Buster for the frontend, Node: Alpine for the backend, mongoDB, and Apache.
+        Terraform is used to build and destroy the AWS environment and docker
+        compose is use to automate the build and run of the containers.
+      </div>
+    </div>
+  );
 };
 
 export default ProjectPage;
