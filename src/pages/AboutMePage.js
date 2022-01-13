7909f22523f0c8c2740654855cf58ef4b470b89d
Eastern Lai <easternlai@Easterns-MacBook-Pro.local>
2022-01-13 14:55:04 -0800

    completed first draft

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -7,29 +7,29 @@ import buddy from "../images/buddy.JPG";
 const AboutMePage = () => {
   const images = [buddy, bingbing, suzie, marty];
   return (
-    <div className="about">
+    <div className="about text-sm">
       <div>
         <span className="bold">contact: </span>
         <span className="mb-10 italic">
           eastern.lai@gmail.com | 415.420.1297
         </span>
       </div>
-      <div>
-        <span className="bold">GitHub: </span>
-        <a href="https://github.com/easternlai">
+      <div className="">
+        <span className="bold">github: </span>
+        <a className="text-x-sm" href="https://github.com/easternlai">
           https://github.com/easternlai
         </a>
       </div>
       <div>
-        <span className="bold">LinkedIn: </span>
-        <a href="https://www.linkedin.com/in/eastern-lai-3b05b585/">
+        <span className="bold">linkedin: </span>
+        <a className="text-x-sm" href="https://www.linkedin.com/in/eastern-lai-3b05b585/">
           https://www.linkedin.com/in/eastern-lai-3b05b585/
         </a>{" "}
       </div>
       <div className="text-large">me</div>
       <div className="mt-20">
         I try to spend my free time on weekends staying connected with family and
-        maintaining friendships that have lasted decades. I also like to explore new walks in different
+        maintaining friendships that have lasted decades. I also like to explore new walk in different
         neighborhoods with my little friends and do my best to make sure they are living
         their best lives.
       </div>
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -7,33 +7,43 @@ const ProjectPage = () => {
       <div className="text-large mb-10">Cultree Project</div>
       <div className="text-med">about </div>
       <div className="projects-page__wrapper">
-      <div className="ml-15">
-        Cultree is a multi-tenant, mern application to helps companies improve
-        their employee engagement with event signups, swag store, and company
-        photo galleries with many more features to come!
-      </div>
-      <img src={cultree} className="projects-page__wrapper--image"/>
-      </div>
+        <div className="ml-15">
+          <div>
+            Cultree is a multi-tenant, mern application to helps companies
+            improve their employee engagement with event signups, swag store,
+            and company photo galleries with many more features to come!
+          </div>
+          <div className="mt-20">
+            <a
+              target="_blank"
+              href="http://ec2-34-221-182-244.us-west-2.compute.amazonaws.com/demologin/dscott@demo-co.com/Password123!"
+            >
+              Demo Cultree as Admin
+            </a>
+          </div>
 
+          <div className></div>
+        </div>
+        <img src={cultree} className="projects-page__wrapper--image" />
+      </div>
 
       <div className="text-med  mt-20">Technologies </div>
       <div className="ml-15 mb-10">
-        <span className="bold">frontend - </span>The frontend was built with
-        ReactJs and uses SCSS for styling. Redux is used to manage the global
-        state and the useReducer and useState hooks are used for component state
-        management as needed.
+        <span className="bold">frontend - </span>reactjs, scss, redux, socketio,
+        react hooks (useState, useReducer, useRef, useEffect).
       </div>
       <div className="ml-15 mb-10">
-        <span className="bold">backend - </span>
+        <span className="bold">backend - </span>nodejs, mongodb, express,
+        socketio, aws s3
+      </div>
+      <div className="ml-15 mb-10">
+        <span className="bold">devops - </span> linux, aws (ec2, s3, vpc),
+        terraform, docker containerization, nginx
+      </div>
+      <div className="ml-15 mb-10">
+        <span className="bold">github - </span> <a href="https://github.com/easternlai/cultree" target="_blank">https://github.com/easternlai/cultree</a>
+      </div>
 
-      </div>
-      <div className="ml-15 mb-10">
-        <span className="bold">devops - </span>
-        Cultree's infrastructure resides in an AWS Linux EC2 instance that
-        maintains  four docker containers: Node:Buster for the frontend, Node: Alpine for the backend, mongoDB, and Apache.
-        Terraform is used to build and destroy the AWS environment and docker
-        compose is use to automate the build and run of the containers.
-      </div>
     </div>
   );
 };
