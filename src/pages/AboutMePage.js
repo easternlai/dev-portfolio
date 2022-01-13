cb5677c2719ce314a786cfc2c2a36c71568feac0
Eastern Lai <easternlai@Easterns-MacBook-Pro.local>
2022-01-12 20:45:27 -0800

    added careers pages

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,21 +1,42 @@
 import React from "react";
-import suzie from '../images/suzie.jpeg';
-import bingbing from '../images/bingbing.JPG';
-import marty from '../images/marty.JPG';
-import buddy from '../images/buddy.JPG';
+import suzie from "../images/suzie.jpeg";
+import bingbing from "../images/bingbing.JPG";
+import marty from "../images/marty.JPG";
+import buddy from "../images/buddy.JPG";
 
 const AboutMePage = () => {
-
-    const images = [buddy, bingbing, suzie, marty ];
+  const images = [buddy, bingbing, suzie, marty];
   return (
     <div className="about">
+      <div>
+        <span className="bold">contact: </span>
+        <span className="mb-10 italic">
+          eastern.lai@gmail.com | 415.420.1297
+        </span>
+      </div>
+      <div>
+        <span className="bold">GitHub: </span>
+        <a href="https://github.com/easternlai">
+          https://github.com/easternlai
+        </a>
+      </div>
+      <div>
+        <span className="bold">LinkedIn: </span>
+        <a href="https://www.linkedin.com/in/eastern-lai-3b05b585/">
+          https://www.linkedin.com/in/eastern-lai-3b05b585/
+        </a>{" "}
+      </div>
       <div className="text-large">me</div>
-      <div className="mb-10 italic">eastern.lai@gmail.com | 415.420.1297</div>
-      <div className="mt-20">Originally from Missouri, I have lived up and down California and currently call the SF north bay home.
-      When I'm not youtubing tech videos I like to explore new walks and do my best to make sure my little friends are living their best lives.
+      <div className="mt-20">
+        I try to spend my free time on weekends staying connected with family and
+        maintaining friendships that have lasted decades. I also like to explore new walks in different
+        neighborhoods with my little friends and do my best to make sure they are living
+        their best lives.
       </div>
       <div className="about__photos">
-        {images.map((image) => <img className="about__photos--photo" src={image}/>)}
+        {images.map((image) => (
+          <img className="about__photos--photo" src={image} />
+        ))}
       </div>
     </div>
   );
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -3,7 +3,17 @@ import React from 'react';
 const ProjectPage = () => {
 
     return (
-        <div>ProjectPage</div>
+        <div>
+        <div className="text-large mb-10">Cultree Project</div>
+        <div className="text-med">about </div>
+        <div className='ml-15'>Cultree is a multi-tenant, mern application that provides a central web location for office managers and company cultree committee liasons to create event signups, offer swag and gifts via a store, and post company photo galleries with many more features to come! </div>
+
+        <div className="text-med  mt-20">Technologies </div>
+        <div className='ml-15 mb-10'><span className="bold">frontend - </span> </div>
+        <div className='ml-15 mb-10'><span classNams="bold">backend - </span>  </div>
+        <div className='ml-15 mb-10'><span className="bold">devops - </span>Cultree's infrastructure resides in an AWS  Linux EC2 instance that maintains the frontend, backend, mongoDB, and apache containers. Terraform is used to build and destroy the AWS environment and docker compose is use to automate the build and run of the containers.</div>
+
+        </div>
     )
 };
 
